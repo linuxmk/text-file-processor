2aeb8bc8f7467efc6cbfb46c5fe6b37234577d0d
Zlatko <linuxmk@gmail.com>
2020-10-13 16:09:21 +0200

    update

--- include/parsetext.h
+++ include/parsetext.h
@@ -11,23 +11,6 @@ using namespace std;
 
 #include "parseargs.h"
 
-template<typename A, typename B>
-std::pair<B,A> flip_pair(const std::pair<A,B> &p)
-{
-    return std::pair<B,A>(p.second, p.first);
-}
-
-template<typename A, typename B>
-auto flip_map(const std::map<A,B> &src)
-{
-    std::multimap<B,A> dst;
-
-    std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()),
-                   flip_pair<A,B>);
-    return dst;
-}
-
-
 class TextParser
 {
 public:
